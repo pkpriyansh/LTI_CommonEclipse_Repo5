43832d85d4f8617c15e1291124c5a55714b5ed50
pkpriyansh <priyansh.pk.1997@gmail.com>
2020-09-10 17:19:43 +0530

    new changes

--- TestCommonGit/src/com/lti/Login.java
+++ TestCommonGit/src/com/lti/Login.java
@@ -1,6 +1,13 @@
 package com.lti;
 
 public class Login {
-	
-	
+	public void login(String username, int password){
+		String user1="priyansh";
+		int pass= 1254;
+		if(username.equals(user1) && password==pass)
+			System.out.println("login successful");
+		else
+			System.out.println("invalid login id or password");
+		
+	}
 }
